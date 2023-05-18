6f7c48939822ee1e933b83df20812a01a4e4ed0e
Syed Mohammad Yasir <smy329@gmail.com>
2023-05-18 11:52:04 +0600

    navbar done

---
+++ data.md
@@ -0,0 +1,10 @@
+Disney Toys Sub-Category:
+
+1. Anime
+2. Fantasy
+3. Horror
+4. Tv Show
+5. Movie
+6. Superhero
+7. Sports
+8. Music
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-
 import './App.css';
+import Navbar from './pages/shared/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-5xl font-bold">Hello Disney Palace</h1>
       <button className="bg-primary p-10 hover:bg-secondary">
         {' '}
